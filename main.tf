8bc75255784283f995e83b39e9ec741be40c7329
hclpocgit <95856172+hclpocgit@users.noreply.github.com>
2021-12-09 18:48:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,24 +7,15 @@ provider "aws" {
 
 resource "aws_instance" "testtcec" {
   ami                    = "ami-07d280422a9f5ee93"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.large"
   key_name               = "shailendra-mumbai-hclawsplm"
   monitoring             = true
   vpc_security_group_ids = ["sg-0df4b19389e5a3734"]
   subnet_id              = "subnet-030516bd5eb774591"
 #vpc_id = "vpc-0023f24693d508a16"
   tags = {
-    Name = "testtcec"
+    Name = "lmco-tc-poc"
     Terraform   = "true"
     Environment = "dev"
   }
 }
-
-# resource "aws_vpc" "test-vpc-ss" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
-
-#   tags = {
-#     Name = "testvpc-sstag"
-#   }
-# }
