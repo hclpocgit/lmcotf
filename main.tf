fb8eba3f683e759edad9eea85e5f342783ab9a87
hclpocgit <95856172+hclpocgit@users.noreply.github.com>
2021-12-13 13:31:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
   # secret_key = "0TdVOz565mRUwjXTUhCzTq4ddPNzpK/+a22MpN1V"
 }
 
-resource "aws_instance" "lmco-tc13-poc" {
+resource "aws_instance" "lmco-tc13-awspoc" {
   ami                    = "ami-07d280422a9f5ee93"
   instance_type          = "t3.large"
   key_name               = "shailendra-mumbai-hclawsplm"
@@ -14,7 +14,7 @@ resource "aws_instance" "lmco-tc13-poc" {
   subnet_id              = "subnet-030516bd5eb774591"
 #vpc_id = "vpc-0023f24693d508a16"
   tags = {
-    Name = "lmco-tc13-poc"
+    Name = "lmco-tc13-awspoc"
     Terraform   = "true"
     Environment = "dev"
   }
